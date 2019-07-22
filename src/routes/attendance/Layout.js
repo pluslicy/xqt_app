cf33338590506383612b17370be6cbedc82915d5
刘亚蓉 <904105440@qq.com>
2019-07-22 20:32:48 +0800

    提交周统计页面数据

--- src/routes/attendance/Layout.js
+++ src/routes/attendance/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavBar,Tabs,Grid, List,SegmentedControl,Toast,Modal } from 'antd-mobile';
+import {NavBar,Tabs,Grid,List,Toast,Modal,Tag,NoticeBar,Icon } from 'antd-mobile';
 import { Chart,Geom,Axis,Tooltip,Coord,Guide } from 'bizcharts';
 import DataSet from "@antv/data-set";
 import axios from '../../http';
@@ -19,7 +19,7 @@ class Layout extends React.Component {
     super(props);
     this.state = {
       // 实时统计总数据
-      realTimeTotalData:{},
+      realTimeTotalData:{total:0},
       // 模态框的显示与隐藏
       modal:false,
       // 模态框标题
@@ -27,7 +27,7 @@ class Layout extends React.Component {
       // 统计选项卡
       tabs:[
         { title: '实时统计'},
-        { title: '周月统计'},
+        { title: '周统计'},
       ],
       // 实时统计图表数据
       realTimeData:[{
@@ -52,46 +52,39 @@ class Layout extends React.Component {
         name:'严重迟到'
       }],
       // 默认周月显示的选中状态  周/月
-      timeType:'周',
-      // 周月统计
+      // timeType:'周',
+      // 周统计
       weekMonthData:[{
+        name:'未打卡',
+        num:0,
+      },{
         name:'迟到',
-        num:23,
+        num:0,
       },{
         name:'早退',
-        num:27,
+        num:0,
       },{
-        name:'缺卡',
-        num:45,
-      },{
-        name:'旷工',
-        num:10,
+        name:'严重迟到',
+        num:0,
       }],
-      // 实时统计或者周月统计某一状态详细数据，比如迟到人详细数据
+      // 实时统计或者周统计某一状态详细数据，比如迟到人详细数据
       detailsData:[],
     };
   }
   componentDidMount(){
     this.findRealTimeData();
   }
-  //查找数据
+  //查找实时统计数据
   findRealTimeData(){
-    Toast.loading('加载中...', 300, () => {
-      // console.log('Load complete !!!');
-    },false);
+   /*  Toast.loading('加载中...', 30, () => {
+      console.log('Load complete !!!');
+    }); */
     axios.get('/attendace/getAttendanceResult').then((res)=>{
-      if(res.status===200){
+      /* if(res.status===200){
         Toast.hide();
-      }
-      console.log(res);
-      // res.data是后台给的所有的数据
-      // 拦截器处理之后，res.data就是后台返回的数组数据
-      // this.setState({
-      //   // courses:res.data.data
-      //   courses:res.data
-      // })
-      // this.realTimeTotalData
+      } */
       this.setState({
+        realTimeTotalData:res.data,
         realTimeData:[{
           item: "打卡人数",
           count: res.data.attendance
@@ -117,100 +110,88 @@ class Layout extends React.Component {
       console.log(error);
     });
   }
-  // 某个状态的详细数据
-  /* detailsData = [
-    {title: '张三' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三2' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三2' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三2' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三2' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三3' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '张三4' },
-    {title: '李四' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-    {title: '李司丞' },
-    {title: '张小敬' },
-    {title: '王五溜溜' },
-  ].map(obj => ({
-    icon: <div style={{lineHeight:'10%',textAlign:'center'}}>{obj.title}</div>,
-    // title: obj.title,
-  })); */
+  //查找周统计数据
+  findWeekTimeData(){
+    axios.get('/attendace/getAttendanceResult').then((res)=>{
+      this.setState({
+        realTimeTotalData:res.data,
+        weekMonthData:[{
+          name:'未打卡',
+          num:0,
+        },{
+          name:'迟到',
+          num:0,
+        },{
+          name:'早退',
+          num:0,
+        },{
+          name:'严重迟到',
+          num:0,
+        }],
+      });
+    }).catch((error)=>{
+      console.log(error);
+    });
+  }
   // 点击了tab键，向后台发送请求
   tabClick = (tab, index)=>{
     console.log(tab);
     if(index===1){
       this.setState({
-        timeType:'周'
+        // timeType:'周'
       });
+    }else{
+      this.findRealTimeData();
     }
   }
   // 点击周，月更改时间类型
-  timeTypeChange = (type,event)=>{
+  /* timeTypeChange = (type,event)=>{
     console.log(type);
     this.setState({
       timeType:type
     });
-  }
+  } */
   // 查看考勤详情
   showDetails = (staticType,checkType,event)=>{
     console.log(staticType,checkType);
+    var dData = [];
+    if(staticType==='实时统计'){
+      if(checkType==='未打卡'){
+        dData = this.state.realTimeTotalData.notSingned;
+      }
+      if(checkType==='迟到'){
+        dData = this.state.realTimeTotalData.late;
+      }
+      if(checkType==='早退'){
+        dData = this.state.realTimeTotalData.early;
+      }
+      if(checkType==='严重迟到'){
+        dData = this.state.realTimeTotalData.serious;
+      }
+    }
+    if(staticType==='周统计'){
+      /* if(checkType==='未打卡'){
+        dData = this.state.realTimeTotalData.notSingned;
+      }
+      if(checkType==='迟到'){
+        dData = this.state.realTimeTotalData.late;
+      }
+      if(checkType==='早退'){
+        dData = this.state.realTimeTotalData.early;
+      }
+      if(checkType==='严重迟到'){
+        dData = this.state.realTimeTotalData.serious;
+      } */
+    }
+    /* console.log(dData.indexOf('高慧银'));
+    if(dData.indexOf('高慧银')!==-1){
+      dData.splice(dData.indexOf('高慧银'),1);
+    } */
     this.setState({
       modal:true,
-      modalTitle:staticType+'-'+checkType
+      modalTitle:staticType+'-'+checkType,
+      detailsData:dData
     });
-    /* let trueData = [];
-    for(var i=0;i<this.detailsData.length;i+=15){
-      trueData.push(this.detailsData.slice(i,i+15));
-    }
-    ActionSheet.showShareActionSheetWithOptions({
-      options: trueData,
-      // title: 'title',
-      message: staticType+'-'+checkType+'详细数据',
-      cancelButtonText:'取消'
-    },
-    (buttonIndex) => {
-      this.setState({ clicked1: buttonIndex > -1 ? this.dataList[buttonIndex].title : '取消' });
-      // also support Promise
-      return new Promise((resolve) => {
-        resolve();
-      });
-    }); */
   }
   // 模态框关闭
   onClose = () => {
@@ -218,7 +199,6 @@ class Layout extends React.Component {
       modal: false,
     });
   }
-  
   onWrapTouchStart = (e) => {
     // fix touch to scroll background page on iOS
     if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
@@ -247,7 +227,7 @@ class Layout extends React.Component {
         }
       }
     };
-    const htmlStr = `<div style="color:#8c8c8c;font-size:1.16em;text-align: center;width: 10em;line-height:3em;">打卡人数/实到人数<br><span style="color:#008AE6;font-size:2.5em;">`+this.state.realTimeData[0].count+`/`+(this.state.realTimeData[0].count+this.state.realTimeData[1].count)+`</span></div>`;
+    const htmlStr = `<div style="color:#8c8c8c;font-size:1.16em;text-align: center;width: 10em;line-height:3em;">打卡人数/实到人数<br><span style="color:#008AE6;font-size:2.5em;">`+this.state.realTimeData[0].count+`/`+this.state.realTimeTotalData.total+`</span></div>`;
     return (
      <div className="check-tab">
        <NavBar leftContent=""> 考勤信息 </NavBar>
@@ -305,7 +285,7 @@ class Layout extends React.Component {
               <Grid hasLine={false} data={this.state.checkTypeData}
                 columnNum={4}
                 renderItem={item => (
-                  <div style={{ padding: '0 12.5px' }} onClick={this.showDetails.bind(this,'实时统计',item.name)}>
+                  <div style={{ padding: '0 12.5px' }} onClick={(item.num>0?(this.showDetails.bind(this,'实时统计',item.name)):null)}>
                     <div style={{ color: '#008AE6', fontSize: '30px'}}>
                       <span>{item.num}</span>
                     </div>
@@ -317,24 +297,27 @@ class Layout extends React.Component {
               />
             </div>
           </div>
-          {/* 周月统计 */}
+          {/* 周统计 */}
           <div style={{backgroundColor: '#fff',paddingTop:'10px',paddingBottom:'10px' }}>
-            <div className="time-type" style={{padding:'0 10px 10px'}}>
+            {/* <div className="time-type" style={{padding:'0 10px 10px'}}>
               <div >
                 <SegmentedControl values={['周', '月']} onValueChange={this.timeTypeChange}
                 style={{ height: '40px'}} />
               </div>
-            </div>
+            </div> */}
+            <NoticeBar style={{height:'50px',lineHeight:'50px',fontSize:'16px'}} icon={<Icon style={{marginRight:'4px'}} type="check-circle-o" size="md" />}>
+              总打卡次数：234次
+            </NoticeBar>
             {/* 统计信息 */}
             <div className="time-details">
-              <div style={{height:'50px',backgroundColor:'#F5F5F9',lineHeight:'50px',paddingLeft:'10px',color:'#777'}}>46人出勤</div>
+             {/*  <div style={{height:'50px',backgroundColor:'#F5F5F9',lineHeight:'50px',paddingLeft:'10px',color:'#777'}}>49人出勤</div> */}
               {/* 出勤详细信息 */}
               <div>
                 <List className="date-picker-list" style={{ backgroundColor: 'white',marign:'10px' }}>
                   {        
                     this.state.weekMonthData.map((item,index) => {
-                      // return <Item key={index} extra={item.num+(index===0?'小时':'人')}arrow="horizontal" onClick={this.showDetails.bind(this,'周月统计',item.name)}>{item.name}</Item>;
-                      return <Item key={index} extra={item.num+'人'}arrow="horizontal" onClick={this.showDetails.bind(this,'周月统计',item.name)}>{item.name}</Item>;
+                      // return <Item key={index} extra={item.num+(index===0?'小时':'人')}arrow="horizontal" onClick={this.showDetails.bind(this,'周统计',item.name)}>{item.name}</Item>;
+                      return <Item key={index} extra={item.num+'次'}arrow="horizontal" onClick={(item.num>0?(this.showDetails.bind(this,'周统计',item.name)):null)}>{item.name}</Item>;
                     })
                   }
                 </List>
@@ -352,8 +335,21 @@ class Layout extends React.Component {
           footer={[{ text: '关闭', onPress: () => { this.onClose()} }]}
           wrapProps={{ onTouchStart: this.onWrapTouchStart }}
         >
-          <div style={{ height: '500px', overflow: 'scroll' }}>
-            <Grid data={this.state.detailsData} activeStyle={false} columnNum={3}/>
+          <div style={{ height: '300px', overflow: 'scroll' }}>
+            <Grid data={this.state.detailsData}
+                columnNum={4}
+                renderItem={item => (
+                  <div style={{ padding: '0 12px' }}>
+                    <div style={{ color: 'white', fontSize: '24px',background:'#008AE6'}}>
+                      <span>{item.slice(item.length-2)}</span>
+                    </div>
+                    <div style={{ color:'#8c8c8c',fontSize: '16px', marginTop: '14px' }}>
+                      {/* <span>{item}</span> */}
+                      <Tag selected>{item}</Tag>
+                    </div>
+                  </div>
+                )}
+              />
           </div>
         </Modal>
      </div> 
