b0338cc693ce982fc5f27b817f3840149c80b476
plusyuyu <532323119@qq.com>
2019-07-22 19:35:01 +0800

    解决视频播放报错问题,但是iframe占用资源问题还未处理

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import style from './login.css';
+import { List, InputItem, Button,WhiteSpace ,Toast} from 'antd-mobile';
 
 class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+        user:'',
+        password:''
+    }
+  }
+  handleChange(key,val) {
+    this.setState({[key]:val})
+  }
+
+  handleSubmit(){
+    Toast.loading('登录中',3,()=>{
+      console.log('关闭了')
+    })
+  }
+
+
   render(){
     return (
       <div style={{position:"absolute",top:0,bottom:0,width:'100%'}}>
-        登录
+        <div className={style.bg}>
+          农大校企通
+        </div>
+        <div>
+          <List>
+            <InputItem
+              onChange={this.handleChange.bind(this,'user')}
+              clear
+              placeholder="">用户名</InputItem>
+            <InputItem
+              onChange={this.handleChange.bind(this,'password')}
+              type="password"
+              clear
+              placeholder="" >密码</InputItem>
+            <WhiteSpace/>
+            <Button onClick={this.handleSubmit.bind(this)} type="primary" style={{marginLeft:'.5em',marginRight:'.5em'}}>登录</Button>
+          </List>
+
+        </div>
       </div>
     )
   }
--- src/routes/video/Layout.js
+++ src/routes/video/Layout.js
@@ -3,26 +3,10 @@ import {NavBar} from 'antd-mobile'
 
 class Layout extends React.Component {
 
-  clearIframe(id){
-    var el = document.getElementById(id),
-    iframe = el.contentWindow;
-    if(el){
-        console.log(el);
-        el.src = 'about:blank';
-        try{
-            iframe.document.write('');
-            iframe.document.clear();
-        }catch(e){
-
-        };
-        //移除 
-        document.getElementById("cc").removeChild(el);
-    }
+  constructor(props){
+    super(props);
   }
 
-  componentWillUnmount(){
-    this.clearIframe('videoFrame')
-  }
 
   render(){
     return (
